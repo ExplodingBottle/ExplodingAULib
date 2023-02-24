0e5942595ab7ee7a62bb8715bb1a501541840922
ExplodingBottle <72459125+ExplodingBottle@users.noreply.github.com>
2023-02-24 17:08:24 +0100

    Removed debug code leftover

--- src/io/github/explodingbottle/explodingau/ExplodingAUWrapper.java
+++ src/io/github/explodingbottle/explodingau/ExplodingAUWrapper.java
@@ -12,16 +12,10 @@ public class ExplodingAUWrapper {
 
 	public static void main(String[] args) {
 		if (args.length == 2) {
-			System.out.println("Two arguments: OK");
 			File override = new File(args[0]);
 			if (override.exists() && override.isFile()) {
-				System.out.println("File exists: OK");
 				ExplodingAULib.standardProgramRoutine(args[1], override);
-			} else {
-				System.out.println("File exists: FAIL");
 			}
-		} else {
-			System.out.println("Two arguments: FAIL");
 		}
 	}
 
